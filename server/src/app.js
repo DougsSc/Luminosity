e11de2dc4a97dd2ea9adbfaeb839c9017ad48801
Marcelo Nicaretta <marcelo.nicaretta@universo.univates.br>
2020-11-22 20:55:59 -0300

    Implementada persistencia dos dados

--- server/src/app.js
+++ server/src/app.js
@@ -1,45 +1,70 @@
 require('dotenv').config()
 const express = require('express')
 const json = require('body-parser').json
+const fs = require('fs')
 
 const app = express()
 const port = process.env.PORT || 9000
 
-const PADDLE_TIME = 5000;
+const DATA_FILE = 'data.json'
 
-const data = []
+const data = {
+  position: 1,
+  data: []
+}
 const actions = []
+
+function saveData() {
+  fs.writeFileSync(DATA_FILE, JSON.stringify(data))
+}
+
+function readData() {
+  data = JSON.parse(fs.readFileSync)
+}
+
+if (!fs.existsSync(DATA_FILE)) {
+  saveData()
+}
 
 app.use(json())
 
 app.get('/data', (req, res) => {
-  res.json(data);
+  readData()
+  res.json(data)
 })
 
 app.post('/data', (req, res) => {
   data.push(req.body)
+  saveData()
   res.sendStatus(200)
 })
 
 app.post('/action', (req, res) => {
-  const action = req.body;
+  const action = req.body
 
-  switch (action.type) {
-    case 'turnPaddles':
-      const next = {
-        motor: 'paddles',
-        direction: action.value < 0 ? 'right' : 'left',
-        time: Math.abs(action.value * PADDLE_TIME)
-      }
-      actions.unshift(next)
-      res.json(next)
-      break
+  if (action.value !== 0) {
+    readData()
+    const nextPosition = Math.max(Math.min(data.position + action.value, 1), 0)
+
+    const next = {
+      value: nextPosition - data.position
+    }
+
+    actions.unshift(next)
+    res.json(next)
   }
   res.sendStatus(400)
 })
 
 app.get('/action', (req, res) => {
-  res.json(actions.pop() ?? null);
+  const action = actions.pop()
+  if (action) {
+    data.position = data.position + action.value
+    saveData()
+    res.json(action)
+  }
+
+  res.sendStatus(200)
 })
 
 app.listen(port, () => {
